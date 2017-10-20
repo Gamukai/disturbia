f79d89cb2656bb02bb0fe3181d18c2d55113000b
Augusto Falcao <falcaoaugustos@gmail.com>
2017-10-20 11:38:44 -0300

    feat (scenes): create cutscene prologue files

---
+++ DisturbiaGame/Scenes/CutscenePrologue/CutscenePrologue.h
@@ -0,0 +1,13 @@
+//
+//  CutscenePrologue.h
+//  DisturbiaGame
+//
+//  Created by Augusto Falcão on 10/20/17.
+//  Copyright © 2017 Augusto Falcão. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CutscenePrologue : NSObject
+
+@end
---
+++ DisturbiaGame/Scenes/CutscenePrologue/CutscenePrologue.m
@@ -0,0 +1,13 @@
+//
+//  CutscenePrologue.m
+//  DisturbiaGame
+//
+//  Created by Augusto Falcão on 10/20/17.
+//  Copyright © 2017 Augusto Falcão. All rights reserved.
+//
+
+#import "CutscenePrologue.h"
+
+@implementation CutscenePrologue
+
+@end
