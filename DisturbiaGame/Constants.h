9ad6cbbd42fff83cc62390ae8e8184e56865cb5d
Augusto Falcao <falcaoaugustos@gmail.com>
2017-10-20 00:02:43 -0300

    feat (constants): create global constants file

---
+++ DisturbiaGame/Constants.h
@@ -0,0 +1,18 @@
+//
+//  Constants.h
+//  DisturbiaGame
+//
+//  Created by Augusto Falcão on 10/20/17.
+//  Copyright © 2017 Augusto Falcão. All rights reserved.
+//
+
+#ifndef DGConstants_h
+#define DGConstants_h
+
+typedef NS_OPTIONS(NSUInteger, CollisionCategory) {
+    heroType    = (1 << 0),
+    terrainType = (1 << 1),
+    pipeType    = (1 << 2)
+};
+
+#endif /* Constants_h */
