673ca3d2963eb57a2a544639c9372dd21cf47594
Augusto Falcao <falcaoaugustos@gmail.com>
2017-10-30 16:07:32 -0300

    feat (components): create pickup files

---
+++ DisturbiaGame/Components/Pickup/Pickup.h
@@ -0,0 +1,13 @@
+//
+//  Pickup.h
+//  DisturbiaGame
+//
+//  Created by Augusto Falcão on 10/30/17.
+//  Copyright © 2017 Augusto Falcão. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Pickup : NSObject
+
+@end
---
+++ DisturbiaGame/Components/Pickup/Pickup.m
@@ -0,0 +1,13 @@
+//
+//  Pickup.m
+//  DisturbiaGame
+//
+//  Created by Augusto Falcão on 10/30/17.
+//  Copyright © 2017 Augusto Falcão. All rights reserved.
+//
+
+#import "Pickup.h"
+
+@implementation Pickup
+
+@end
